9cdc664289bace78a08a02796dce9e2fd9d0a876
Keerthi7418 <149948476+Keerthi7418@users.noreply.github.com>
2024-03-12 23:26:49 +0530

    Delete springbootasssignment directory

--- springbootasssignment/src/main/java/com/test/springbootasssignment/repository/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.springbootasssignment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.springbootasssignment.model.Role;
-
-public interface RoleRepo extends JpaRepository<Role, Integer>{
-
-}
